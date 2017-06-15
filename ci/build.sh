bed17cd6b7264d480e96db45b3bbbd07edc2b6c3
Marcelo Mandolesi <mmandolesi@gannett.com>
2017-06-14 21:41:54 -0400

    Adding concourse files

---
+++ ci/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e -u -x
+
+yum install kernel-{devel,headers}-3.10.0-514.6.2.el7.x86_64 -y
+yum install git gcc make rpm-build -y
+cd amzn-drivers/kernel/linux/rpm/
+make
+rpmbuild  --rebuild ena-1.1.3-1.el7.centos.src.rpm
+ls -hal /root/rpmbuild/RPMS/x86_64
+echo "${gdcustom-key}" > .gdcustom-ssh.key
+chmod 400 gdcustom-ssh.key
+scp -i gdcustom-ssh.key -o "StrictHostKeyChecking no" /root/rpmbuild/RPMS/x86_64/*.rpm yumrepo@10.84.115.4:/repos/gdcustom-test/7/x86_64/Packages/
+ssh -i gdcustom-ssh.key -o "StrictHostKeyChecking no" yumrepo@10.84.115.4 'createrepo --update /repos/gdcustom-test/7/x86_64'
