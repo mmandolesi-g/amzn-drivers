3c00726f559bf5aada5ffcd37f93c647f90f683a
Marcelo Mandolesi <mmandolesi@gannett.com>
2017-06-14 22:05:08 -0400

    Update to CI files

--- ci/build.sh
+++ ci/build.sh
@@ -8,9 +8,7 @@ cd kernel/linux/rpm/
 make
 rpmbuild  --rebuild ena-1.1.3-1.el7.centos.src.rpm
 ls -hal /root/rpmbuild/RPMS/x86_64
-env
 echo "${gdcustom_key}" > gdcustom-ssh.key
 chmod 400 gdcustom-ssh.key
-cat gdcustom-ssh.key
 scp -i gdcustom-ssh.key -o "StrictHostKeyChecking no" /root/rpmbuild/RPMS/x86_64/*.rpm yumrepo@10.84.115.4:/repos/gdcustom-test/7/x86_64/Packages/
 ssh -i gdcustom-ssh.key -o "StrictHostKeyChecking no" yumrepo@10.84.115.4 'createrepo --update /repos/gdcustom-test/7/x86_64'
